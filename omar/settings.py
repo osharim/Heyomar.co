b3e63c73b8794b732999e221e87eb689498d4076
omar <omarisel01@gmail.com>
2013-02-28 18:44:18 -0600

    fixing db

--- omar/settings.py
+++ omar/settings.py
@@ -39,7 +39,7 @@ else:
         'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'bindsmec_omar',                      # Or path to database file if using sqlite3.
-        'USER': 'bindsmec_omr',                      # Not used with sqlite3.
+        'USER': 'bindsmec',                      # Not used with sqlite3.
         'PASSWORD': 'nf9ckpgA?',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
