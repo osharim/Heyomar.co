1f23cbeade433eb427d311713bd81cf930b3df40
omar <omarisel01@gmail.com>
2013-02-28 18:54:30 -0600

    agregando a las seccion de skill el routing de viewers

--- apps/skills/views.py
+++ apps/skills/views.py
@@ -2,10 +2,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
- 
+from helpers.core import get_client_ip
+
+
+
+
 def skills_view(request):
  
- 
+  	viewer.objects.create( zone = 2 ,ip = get_client_ip(request ) ) 
+
 	ctx = {
 
 			'skills_is_active' : True ,
