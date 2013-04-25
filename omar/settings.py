203bc98e825cbbba287ac6d28adf9923cf27fb06
omar <omar@lt.(none)>
2013-04-24 19:19:41 -0500

    gunicorn + virtualenv

--- omar/settings.py
+++ omar/settings.py
@@ -2,6 +2,13 @@
 #encoding:utf-8
 import os
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Django settings for tributario project.
 
 DEBUG = True
